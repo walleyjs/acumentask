d80e9771f242dd6f79c5d498ca9b81c450d08c7a
walley <rajiolawale2020@gmai.com>
2021-02-15 16:45:34 +0100

    ionic setup

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 
 
 const Hapi = require('@hapi/hapi');
+const Ejs = require('ejs');
+const Vision = require('@hapi/vision');
 
-const init = async () => {
+const server = Hapi.server({
+    port: 8000,
+    host: 'localhost'
+});
+const rootHandler = (request, h) => {
 
-
-    const server = Hapi.server({
-        port: 8000,
-        host: 'localhost'
+return h.view('index', {
+        title: 'views | Hapi ' + request.server.version,
+        message: 'Hello Ejs!'
     });
+};
+const init = async () => {
+   
+    await server.register(Vision);
+    server.views({
+        engines: { ejs: Ejs },
+        relativeTo: __dirname,
+        path: 'views'
+    });
+
+     server.route({ method: 'GET', path: '/', handler: rootHandler });
 
     const user = {
         cache: { expiresIn: 5000 },
@@ -21,7 +37,7 @@ const init = async () => {
     server.route({ method: 'GET', path: '/user', options: user });
     
     await server.start();
-    console.log('Server running on port 8000');
+    console.log('Server running at:', server.info.uri);
 };
 
 init();
