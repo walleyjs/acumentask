55205d3f89cfa82646db346d37a95725b0701ffd
walley <rajiolawale2020@gmai.com>
2021-02-15 15:42:52 +0100

    f push

--- app.js
+++ app.js
@@ -25,12 +25,3 @@ const init = async () => {
 };
 
 init();
-
-// const Hapi = require('hapi');
-
-// // Create a server with a host and port  
-// const server = Hapi.server({
-//   host: 'localhost',
-//   port: 8000
-// });
-
